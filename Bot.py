380351a23fbc88bf89f7c77f6a1a992371d6dc02
Bernardo Porto Veronese <36862109+BernardoVeronese@users.noreply.github.com>
2018-03-15 02:47:37 -0300

    Add files via upload
    New changes to bot.py

--- Bot.py
+++ Bot.py
@@ -4,10 +4,15 @@ import pygame
 class Bot(object):
 
     # Initialization
-    def __init__(self,x,y,image):
+    def __init__(self,x,y,image,speed):
         self.x = x
         self.y = y
         self.image = image
+        self.in_node = False
+        self.next_node = 0
+        self.directionx = -x
+        self.directiony = -y
+        self.speed = speed
         #self.angle = angle
         #self.speed = speed
 
@@ -17,9 +22,15 @@ class Bot(object):
 
     #Method to move object (special input of dx and dy)
     def follow(self):
+        if self.in_node:
+            #self.next_node = Djikstra algorithm
+
+        #Calculating new direction
+        self.directionx = next_node[0]-x #Fix next_node data type so it has x and y
+        self.directiony = next_node[1]-y
+
+        #Updating course
+        self.x += self.speed*self.directionx/(self.directionx**2+self.directiony**2)**0.5
+        self.y += self.speed*self.directiony/(self.directionx**2+self.directiony**2)**0.5
         
 
-
-    #def move(self, speed, angle)
-        #self.x += math.sin(self.angle) * self.speed
-        #self.y -= math.cos(self.angle) * self.speed
---
+++ GeographyGraph.py
@@ -0,0 +1,13 @@
+import pygame
+
+#Simple player object
+class GeographyGraph(object):
+
+    # Initialization
+    def __init__(self, x, y, image, speed):
+        self.graph = map_graph #dictionary implementation
+
+    # Method to draw object
+    def draw(self):
+        window.blit(self.image, (self.x, self.y))  # mudar para load image algo
+       
