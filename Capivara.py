4def3b40c1384ab73f1513cee9e8cb850db5b9f3
BernardoVeronese <bernardopveronese@gmail.com>
2018-04-25 22:22:49 -0300

    Updated Player, Heli and Capivara classes

--- Capivara.py
+++ Capivara.py
@@ -19,11 +19,6 @@ class Capivara(pygame.sprite.Sprite):
         self.rect.center = (self.x, self.y)
 
 
-    #Method to draw object
-    def draw(self):
-        window.blit(self.image,(self.x,self.y))#mudar para load image algo
-
-
     def state_change(self):
         if self.state:
             self.killer(screen_size)
@@ -35,10 +30,10 @@ class Capivara(pygame.sprite.Sprite):
         return self.rect.colliderect(sprite.rect)
 
 
-    def time_delay(self):
+    def time_delay(self, sprite):
         clock = pygame.time.Clock()
         counter = 0
-        while not self.collided(self, sprite) and counter < 30:
+        while not self.collided(sprite) and counter < 30:
             clock.tick(20)
             counter = counter + 1
 
@@ -49,7 +44,7 @@ class Capivara(pygame.sprite.Sprite):
 
 
     def spawner(self, screen_size):#screen_size addition later
-        x_pos = randint(0, screen_size)
+        x_pos = randint(0, screen_size) #Change
         y_pos = randint(0, screen_size)
         while not verify_street(x_pos, y_pos):#update later
             x_pos = randint(0, screen_size)
@@ -61,21 +56,15 @@ class Capivara(pygame.sprite.Sprite):
 
 
     def killer(self, screen_size):#screen_size addition later
-        x_pos = randint(0, screen_size)
+        x_pos = randint(0, screen_size) #Change
         y_pos = randint(0, screen_size)
         while verify_street(x_pos,y_pos):  # update later
             x_pos = randint(0, screen_size)
             y_pos = randint(0, screen_size)
         self.x = x_pos
         self.y = y_pos
-        self.update_pos(self)
+        self.update_pos()
         self.state = False
 
 
-    def update_pos(self):
-        self.rect = self.image.get_rect()  # Replace old rect with new rect.
-        self.rect.center = (self.x, self.y)  # Put the new rect's center at old center.
 
-
-    def collided(self,sprite):
-        return self.rect.colliderect(sprite.rect)
